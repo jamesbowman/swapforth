f035781df0a0b7a2ff681a577eb08756a048e4f9
James Bowman <jamesb@excamera.com>
2015-08-01 13:07:09 -0700

    First cut of interactive J1a under verilator

---
+++ j1a/verilator/j1a.v
@@ -0,0 +1,84 @@
+`timescale 1 ns / 1 ps
+
+`default_nettype none
+`define WIDTH 16
+
+module j1a(input wire clk,
+           input wire resetq,
+           output wire uart0_wr,
+           output wire uart0_rd,
+           output wire [7:0] uart_w,
+           input wire [7:0] uart0_data
+);
+  wire io_rd, io_wr;
+  wire [15:0] mem_din;
+  wire [15:0] mem_addr;
+  wire mem_wr;
+  wire [15:0] dout;
+  wire [15:0] io_din;
+  /* verilator lint_off UNUSED */
+  wire [12:0] code_addr;
+  /* verilator lint_on UNUSED */
+  wire [15:0] insn;
+
+  reg [15:0] ram_prog[0:2047] /* verilator public_flat */;
+  always @(posedge clk) begin
+    insn <= ram_prog[code_addr[10:0]];
+    if (mem_wr & !mem_addr[12])
+      ram_prog[mem_addr[11:1]] <= dout;
+  end
+
+  // always @(posedge clk) $display("code %x", code_addr);
+
+  reg [15:0] ram_data[0:2047] /* verilator public_flat */;
+  always @(posedge clk) begin
+    mem_din <= ram_data[mem_addr[11:1]];
+    if (mem_wr & mem_addr[12])
+      ram_data[mem_addr[11:1]] <= dout;
+  end
+
+  j1 _j1(
+    .clk(clk),
+    .resetq(resetq),
+    .io_rd(io_rd),
+    .io_wr(io_wr),
+    .mem_wr(mem_wr),
+    .dout(dout),
+    .io_din(io_din),
+    .mem_din(mem_din),
+    .mem_addr(mem_addr),
+    .code_addr(code_addr),
+    .insn(insn));
+
+  // ######   IO SIGNALS   ####################################
+
+  reg io_wr_, io_rd_;
+  /* verilator lint_off UNUSED */
+  reg [15:0] dout_;
+  reg [15:0] io_addr_;
+  /* verilator lint_on UNUSED */
+
+  always @(posedge clk) begin
+    {io_rd_, io_wr_, dout_} <= {io_rd, io_wr, dout};
+    if (io_rd | io_wr)
+      io_addr_ <= mem_addr;
+  end
+
+  // ######   UART   ##########################################
+
+  wire uart0_wr = io_wr_ & io_addr_[12];
+  wire uart0_rd = io_rd_ & io_addr_[12];
+  assign uart_w = dout_[7:0];
+
+  // ######   IO PORTS   ######################################
+
+  /*        bit READ            WRITE
+      1000  12  UART RX         UART TX
+      2000  13  misc.in
+  */
+
+  assign io_din =
+    (io_addr_[12] ? {8'd0, uart0_data}                                  : 16'd0) |
+    (io_addr_[13] ? {12'd0, 1'b0, 1'b0, 1'b1, 1'b1} : 16'd0);
+
+endmodule
---
+++ j1a/verilator/sim_main.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include "Vj1a.h"
+#include "verilated_vcd_c.h"
+
+int main(int argc, char **argv)
+{
+    Verilated::commandArgs(argc, argv);
+    Vj1a* top = new Vj1a;
+
+    // Verilated::traceEverOn(true);
+    // VerilatedVcdC* tfp = new VerilatedVcdC;
+    // top->trace (tfp, 99);
+    // tfp->open ("simx.vcd");
+
+    if (argc != 2) {
+      fprintf(stderr, "usage: sim <hex-file>\n");
+      exit(1);
+    }
+
+    FILE *hex = fopen(argv[1], "r");
+    int i;
+    for (i = 0; i < 2048; i++) {
+      unsigned int v;
+      if (fscanf(hex, "%x\n", &v) != 1) {
+        fprintf(stderr, "invalid hex value at line %d\n", i + 1);
+        exit(1);
+      }
+      top->v__DOT__ram_prog[i] = v;
+    }
+    for (i = 0; i < 2048; i++) {
+      unsigned int v;
+      if (fscanf(hex, "%x\n", &v) != 1) {
+        fprintf(stderr, "invalid hex value at line %d\n", i + 1);
+        exit(1);
+      }
+      top->v__DOT__ram_data[i] = v;
+    }
+
+    // FILE *input = fopen(argv[1], "r");
+    // if (!input) {
+    //   perror(argv[1]);
+    //   exit(1);
+    // }
+    // top->io_din = getc(input);
+
+    top->resetq = 0;
+    top->eval();
+    top->resetq = 1;
+
+    FILE *log = fopen("log", "w");
+    int t = 0;
+    // for (i = 0; /*i < 534563551 */; i++) {
+    for (i = 0; ; i++) {
+      top->clk = 1;
+      top->eval();
+      // tfp->dump(t);
+      t += 20;
+
+      top->clk = 0;
+      top->eval();
+      // tfp->dump(t);
+      t += 20;
+      if (top->uart0_wr) {
+        // printf("out %d\n", top->uart_w);
+        putchar(top->uart_w);
+        putc(top->uart_w, log);
+      }
+      if (top->uart0_rd) {
+        int c = getchar();
+        top->uart0_data = (c == '\n') ? '\r' : c;
+      }
+    }
+    printf("Simulation ended after %d cycles\n", i);
+    delete top;
+    // tfp->close();
+    fclose(log);
+
+    exit(0);
+}
