70d93eccf3993709f8389bfbbe515fb12d8db6c7
jamesbowman <jamesb@excamera.com>
2015-10-14 19:45:49 -0700

    Initial Makefile and test under simulator

---
+++ ft900/expand.py
@@ -0,0 +1,21 @@
+import sys
+
+def expand(filename):
+    for dir in ('.', '../common', '../anstests/'):
+        try:
+            f = open(dir + "/" + filename)
+        except IOError:
+            continue
+        for line in f:
+            line = line.replace('\r', '')
+            if line.strip().startswith('include '):
+                expand(line.split()[1])
+            else:
+                sys.stdout.write(line)
+        print
+        return
+    assert 0, filename + 'not found'
+
+if __name__ == '__main__':
+    for a in sys.argv[1:]:
+        expand(a)
