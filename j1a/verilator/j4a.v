6f4c282c2a9322126f51b261a593cf1217ba639a
Remy Dyer (CSIRO) <dye025@csiro.au>
2015-12-17 08:41:37 +1100

    added verilator support for j4a. build with make -C verilator j4a

---
+++ j1a/verilator/j4a.v
@@ -0,0 +1,91 @@
+`timescale 1 ns / 1 ps
+
+`default_nettype none
+`define WIDTH 16
+
+module j4a(input wire clk,
+           input wire resetq,
+           output wire uart0_wr,
+           output wire uart0_rd,
+           output wire [7:0] uart_w,
+           input wire uart0_valid,
+           input wire [7:0] uart0_data
+);
+  wire io_rd, io_wr;
+  wire [15:0] mem_addr;
+  wire mem_wr;
+  wire [15:0] dout;
+  wire [15:0] io_din;
+  /* verilator lint_off UNUSED */
+  wire [12:0] code_addr;
+  wire [1:0] io_thread;
+  wire [15:0] return_top;
+  wire [3:0] kill_slot_rq;
+
+  /* verilator lint_on UNUSED */
+  wire [15:0] insn;
+   
+  assign kill_slot_rq = 4'b0;
+  
+  reg [15:0] ram_prog[0:4095] /* verilator public_flat */;
+  always @(posedge clk) begin
+    // $display("pc=%x", code_addr * 2);
+    insn <= ram_prog[code_addr[11:0]];
+    if (mem_wr)
+      ram_prog[mem_addr[12:1]] <= dout;
+  end
+
+  j4 _j4(
+    .clk(clk),
+    .resetq(resetq),
+    .io_rd(io_rd),
+    .io_wr(io_wr),
+    .mem_wr(mem_wr),
+    .dout(dout),
+    .io_din(io_din),
+    .mem_addr(mem_addr),
+    .code_addr(code_addr),
+    .insn(insn),
+    .io_slot(io_thread),
+    .return_top(return_top),
+    .kill_slot_rq(kill_slot_rq));
+
+  // ######   IO SIGNALS   ####################################
+
+  reg io_wr_, io_rd_;
+  /* verilator lint_off UNUSED */
+  reg [15:0] dout_;
+  reg [15:0] io_addr_;
+  /* verilator lint_on UNUSED */
+
+  always @(posedge clk) begin
+    {io_rd_, io_wr_, dout_} <= {io_rd, io_wr, dout};
+    if (io_rd | io_wr)
+      io_addr_ <= mem_addr;
+  end
+
+  // ######   UART   ##########################################
+
+  wire uart0_wr = io_wr_ & io_addr_[12];
+  wire uart0_rd = io_rd_ & io_addr_[12];
+  assign uart_w = dout_[7:0];
+
+  // always @(posedge clk) begin
+  //   if (uart0_wr)
+  //     $display("--- out %x %c", uart_w, uart_w);
+  //   if (uart0_rd)
+  //     $display("--- in %x %c", uart0_data, uart0_data);
+  // end
+
+  // ######   IO PORTS   ######################################
+
+  /*        bit READ            WRITE
+      1000  12  UART RX         UART TX
+      2000  13  misc.in
+  */
+
+  assign io_din =
+    (io_addr_[12] ? {8'd0, uart0_data}                                  : 16'd0) |
+    (io_addr_[13] ? {12'd0, 1'b0, 1'b0, uart0_valid, 1'b1} : 16'd0);
+
+endmodule
